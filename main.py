c96764add3ee7e255825e60b9adf11ef990eab23
rafaelsosabrito <142977203+rafaelsosabrito@users.noreply.github.com>
2023-08-25 11:36:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ if st.checkbox("Display"):
   with open('histories_resize_15-15.pkl', 'rb') as file:
       history_list = pickle.load(file)
 
-  # Crear una figura y ejes para el gráfico
-  fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14, 6))
-
   history_acc = []
   history_val_acc = []
   history_loss = []
@@ -35,21 +32,28 @@ if st.checkbox("Display"):
       history_loss.extend(history['loss'])
       history_val_loss.extend(history['val_loss'])
 
+  
+  # Crear una figura y ejes para el gráfico
+  #fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(14, 6))
+  
   # Graficar la precisión (accuracy) en función de las épocas
-  axes[0].plot(history_acc, label='Train Accuracy')
-  axes[0].plot(history_val_acc, label='Validation Accuracy')
-  axes[0].set_title('Accuracy')
-  axes[0].set_xlabel('Epoch')
-  axes[0].set_ylabel('Accuracy')
-  axes[0].legend()
+  #axes[0].plot(history_acc, label='Train Accuracy')
+  #axes[0].plot(history_val_acc, label='Validation Accuracy')
+  #axes[0].set_title('Accuracy')
+  #axes[0].set_xlabel('Epoch')
+  #axes[0].set_ylabel('Accuracy')
+  #axes[0].legend()
 
-  axes[1].plot(history_loss, label='Train Loss')
-  axes[1].plot(history_val_loss, label='Validation Loss')
-  axes[1].set_title('Loss')
-  axes[1].set_xlabel('Epoch')
-  axes[1].set_ylabel('Loss')
-  axes[1].legend()
+  #axes[1].plot(history_loss, label='Train Loss')
+  #axes[1].plot(history_val_loss, label='Validation Loss')
+  #axes[1].set_title('Loss')
+  #axes[1].set_xlabel('Epoch')
+  #axes[1].set_ylabel('Loss')
+  #axes[1].legend()
 
-  plt.tight_layout()
+  #plt.tight_layout()
   #plt.show()
+
+
+  fig = sns.catplot(x='Epoch', y='Accuracy', data=history_val_acc, kind='point')
   st.pyplot(fig)
